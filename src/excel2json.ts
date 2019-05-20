441cd752a0a506e582baabe432a6917a48b62af4
ydl <yangdeliang@juwang.cn>
2019-05-20 17:19:48 +0800

    feat: reformat code

--- src/excel2json.ts
+++ src/excel2json.ts
@@ -1,90 +1,8 @@
 import * as XLSX from 'xlsx';
 import {WorkSheet} from "xlsx";
+import {sheetsMap} from "./sheet-map";
 
 const { utils } = XLSX;
-
-interface Item {
-    name: string
-    map: Array<string>
-}
-
-interface SheetMap {
-    [key: string]: Item
-}
-
-let sheetsMap: SheetMap = {
-    items: {
-        name: "【仅开发用】服物品配置",
-        map: [
-            'weight',
-            'id',
-            'name.cn',
-            'name.tw',
-            'name.hk',
-            'gender',
-            'categoryId',
-            'layerId',
-            'level',
-            'image.thumb',
-            'image.original',
-            'slice.up',
-            'slice.down',
-            'obtain.cn',
-            'obtain.tw',
-            'obtain.hk',
-            'link',
-            'onlineTime',
-            'type',
-            'number',
-            'refItemId',
-        ],
-    },
-    categories: {
-        name: "类别配置",
-        map: [
-            'id',
-            'name.tw',
-            'name.hk',
-            'name.cn',
-            'gender',
-            'slice.normal',
-            'slice.grey',
-        ]
-    },
-    boxes: {
-        name: "【仅开发用】礼盒配置",
-        map: [
-            'weight',
-            'id',
-            'name.cn',
-            'name.tw',
-            'name.hk',
-            'price.oneTimes',
-            'price.tenTimes',
-            'cover.female',
-            'cover.male',
-            'cover.normal',
-            'freeCd',
-            'onlineTime',
-        ]
-    },
-    boxItems: {
-        name: "【仅开发用】礼盒详情配置",
-        map: [
-            'boxId',
-            'itemId',
-        ]
-    },
-    layers: {
-        name: "层级配置",
-        map: [
-            'id',
-            'categoryId',
-            'layer.up',
-            'layer.down',
-        ]
-    },
-}
 
 export interface Hash {
     [key: string]: any
@@ -141,7 +59,7 @@ export class ExcelToJson {
             }
             result.push(datum)
         })
-
+        
         return result
     }
 
